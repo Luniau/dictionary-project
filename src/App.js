e53bc69761a7ce1d7ff08849b460f29d444977d6
Luniau <lubecarvalho@hotmail.com>
2022-05-20 11:45:58 +0200

    added image

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-
-import './App.css';
+import logo from "./logo.jpg";
+import "./App.css";
 import Dictionary from "./Dictionary";
 
 export default function App() {
@@ -7,6 +7,7 @@ export default function App() {
     <div className="App">
       <div className='container'>
       <header className="App-header">
+          <img src={logo} className="App-logo img fluid" alt="logo" />
     </header>
     <main> <Dictionary />
     </main>
